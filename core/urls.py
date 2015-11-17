8fb2157f2db3ffe1b0b106de61a628fbbf5ee3be
wchavers <wachavers@quinnipiac.edu>
2015-11-17 00:55:21 +0000

    Add required user authentication

--- core/urls.py
+++ core/urls.py
@@ -1,16 +1,17 @@
 from django.conf.urls import patterns, include, url
+from django.contrib.auth.decorators import login_required
 from .views import *
 
 urlpatterns = patterns('',
     url(r'^$', Home.as_view(), name='home'),
     url(r'^user/', include('registration.backends.simple.urls')),
     url(r'^user/', include('django.contrib.auth.urls')),
-    url(r'^forum/create/$', ForumCreateView.as_view(), name='forum_create'),
-    url(r'forum/$', ForumListView.as_view(), name='forum_list'),
-    url(r'^forum/(?P<pk>\d+)/$', ForumDetailView.as_view(), name='forum_detail'),
-    url(r'^forum/update/(?P<pk>\d+)/$', ForumUpdateView.as_view(), name='forum_update'),
-    url(r'^forum/delete/(?P<pk>\d+)/$', ForumDeleteView.as_view(), name='forum_delete'),
-    url(r'^forum/(?P<pk>\d+)/answer/create/$', AnswerCreateView.as_view(), name='answer_create'),
-    url(r'^forum/(?P<forum_pk>\d+)/answer/update/(?P<answer_pk>\d+)/$', AnswerUpdateView.as_view(), name='answer_update'),
-    url(r'^forum/(?P<forum_pk>\d+)/answer/delete/(?P<answer_pk>\d+)/$', AnswerDeleteView.as_view(), name='answer_delete'),                   
+    url(r'^forum/create/$', login_required(ForumCreateView.as_view()), name='forum_create'),
+    url(r'forum/$', login_required(ForumListView.as_view()), name='forum_list'),
+    url(r'^forum/(?P<pk>\d+)/$', login_required(ForumDetailView.as_view()), name='forum_detail'),
+    url(r'^forum/update/(?P<pk>\d+)/$', login_required(ForumUpdateView.as_view()), name='forum_update'),
+    url(r'^forum/delete/(?P<pk>\d+)/$', login_required(ForumDeleteView.as_view()), name='forum_delete'),
+    url(r'^forum/(?P<pk>\d+)/answer/create/$', login_required(ForumCreateView.as_view()), name='answer_create'),
+    url(r'^forum/(?P<forum_pk>\d+)/answer/update/(?P<answer_pk>\d+)/$', login_required(AnswerUpdateView.as_view()), name='answer_update'),
+    url(r'^forum/(?P<forum_pk>\d+)/answer/delete/(?P<answer_pk>\d+)/$', login_required(AnswerDeleteView.as_view()), name='answer_delete'),
 )
